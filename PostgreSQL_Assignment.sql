338d8d35e632a8c42b16943397a0d193713073c7
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-06-04 12:39:17 +0600

    added all problem query  and readme file

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,28 +1,69 @@
--- Create rangers table
 
-CREATE TABLE rangers (
-    ranger_id serial PRIMARY KEY, 
-    name VARCHAR(50) NOT NULL,
-    region VARCHAR(50) NOT NULL
-)
 
--- create species table 
+-- SELECT * FROM rangers;
 
-CREATE TABLE species (
-    species_id serial PRIMARY KEY, 
-    common_name VARCHAR(50) NOT NULL,
-    scientific_name VARCHAR(50) NOT NULL,
-    discovery_date date NOT NULL,
-    conservation_status VARCHAR(50) NOT NULL CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Historic'))
-);
+-- SELECT * FROM species;
 
--- create sightings table 
-CREATE TABLE sightings (
-    sighting_id serial PRIMARY KEY, 
-     ranger_id INT NOT NULL,
-    species_id INT NOT NULL,
-    sighting_time TIMESTAMP NOT NULL,
-    notes TEXT
+-- SELECT * FROM sightings;
+
+-- Problem 1 
+INSERT INTO rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+
+-- Problem 2
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+-- Problem 3
+SELECT sighting_id, species_id, ranger_id, location, sighting_time, notes
+FROM sightings
+WHERE location LIKE '%Pass%';
+
+
+-- Problem 4
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name
+ORDER BY r.name;
+
+
+-- Problem 5
+SELECT species.common_name
+FROM species
+LEFT JOIN sightings ON species.species_id = sightings.species_id
+WHERE sightings.species_id IS NULL;
+
+-- Problem 6
+SELECT 
+    species.common_name, 
+    sightings.sighting_time, 
+    rangers.name
+FROM sightings
+JOIN species ON sightings.species_id = species.species_id
+JOIN rangers ON sightings.ranger_id = rangers.ranger_id
+ORDER BY sightings.sighting_time DESC
+LIMIT 2;
+
+-- Problem 7
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+-- Problem 8
+SELECT sighting_id,
+    CASE 
+        WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+        WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS time_of_day
+FROM sightings;
+
+-- Problem 9
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+    SELECT DISTINCT ranger_id FROM sightings
 );
 
 
